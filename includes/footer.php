3f299e53eec06378df1c2b3d127305c4140625ce
Anson W. Han <ansonwhan@gmail.com>
2014-12-04 21:33:47 -0500

    added saving teens logo affiliation

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,8 @@ Phone: <a href="tel:+215-699-1275">610-527-3000</a><br/>
 <a href="<?php ReturnDeepNav(); ?>contact-creative-educational-options.html#contactForm">nancy@creative-ed-options.com</a>
 </p></div>
 <!--<div style="float:left;width:124px;"><a href="http://www.papsy.org/" target="_blank" style="text-decoration:none;border:none;"><img src="<?php ReturnDeepNav(); ?>img/logo-papsy.jpg" alt="PPA" style="padding-left:40px;width:39px;padding-top:15px;"/></a></div>-->
-<div style="float:left;width:170px;"><a href="http://www.aicep.org/" target="_blank" style="text-decoration:none;border:none;"><img src="<?php ReturnDeepNav(); ?>img/logo-cep.jpg" alt="AICEP" style="padding-left:70px;width:45px;padding-top:15px;"/></a></div>
-<div style="float:left;width:124px;padding-left:20px;"><a href="http://www.specialistsofschools.org/" target="_blank" style="text-decoration:none;border:none;"><img src="<?php ReturnDeepNav(); ?>img/logo-sos.jpg" alt="SOS" style="padding-left:30px;width:45px;padding-top:15px;"/></a></div>
+<div style="float:left;width:113px;"><a href="http://www.aicep.org/" target="_blank" style="text-decoration:none;border:none;"><img src="<?php ReturnDeepNav(); ?>img/logo-cep.jpg" alt="AICEP" style="padding-left:40px;width:45px;padding-top:15px;"/></a></div>
+<div style="float:left;width:113px;"><a href="http://www.savingteens.org/" target="_blank" style="text-decoration:none;border:none;"><img src="<?php ReturnDeepNav(); ?>img/savingteensincrisis-logo.png" alt="Saving Teens in Crisis Collaborative" style="padding-left:20px;width:60px;padding-top:8px;"/></a></div>
+<div style="float:left;width:auto;padding-left:0px;"><a href="http://www.specialistsofschools.org/" target="_blank" style="text-decoration:none;border:none;"><img src="<?php ReturnDeepNav(); ?>img/logo-sos.jpg" alt="SOS" style="padding-left:10px;width:45px;padding-top:15px;"/></a></div>
 </div>
 <div style="margin:0;clear:both;font-size:10px;text-align:center;width:100%;">With Offices in Bryn Mawr, PA and Princeton, NJ</div>
