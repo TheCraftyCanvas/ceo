a0442a2afcaed64af047846b69dd6b0d7e153f75
Anson W. Han <ansonwhan@gmail.com>
2014-12-04 20:07:16 -0500

    updated repo, reflecting new footer phone number and offices listing

--- includes/footer.php
+++ includes/footer.php
@@ -3,12 +3,11 @@
 <div style="float:left;width:143px;padding-left:0px;"><a href="http://www.iecaonline.com/" target="_blank" style="text-decoration:none;border:none;"><img src="<?php ReturnDeepNav(); ?>img/logo-ieca.jpg" alt="IECA" style="padding-left:63px;width:80px;padding-top:15px;"/></a></div>
 <div style="float:left;width:231px;padding-bottom:0;margin-bottom:0;padding-left:62px;"><p style="text-align:center;padding-bottom:0;margin-bottom:0;" id="signature">
 <a href="#" target="_self">Nancy V. Greene, M.S.</a><br />
-Phone: <a href="tel:+215-699-1275">215-699-1275</a><br/>
-Mobile: <a href="tel:+610-213-3599">610-213-3599</a><br/>
+Phone: <a href="tel:+215-699-1275">610-527-3000</a><br/>
 <a href="<?php ReturnDeepNav(); ?>contact-creative-educational-options.html#contactForm">nancy@creative-ed-options.com</a>
 </p></div>
 <!--<div style="float:left;width:124px;"><a href="http://www.papsy.org/" target="_blank" style="text-decoration:none;border:none;"><img src="<?php ReturnDeepNav(); ?>img/logo-papsy.jpg" alt="PPA" style="padding-left:40px;width:39px;padding-top:15px;"/></a></div>-->
 <div style="float:left;width:170px;"><a href="http://www.aicep.org/" target="_blank" style="text-decoration:none;border:none;"><img src="<?php ReturnDeepNav(); ?>img/logo-cep.jpg" alt="AICEP" style="padding-left:70px;width:45px;padding-top:15px;"/></a></div>
 <div style="float:left;width:124px;padding-left:20px;"><a href="http://www.specialistsofschools.org/" target="_blank" style="text-decoration:none;border:none;"><img src="<?php ReturnDeepNav(); ?>img/logo-sos.jpg" alt="SOS" style="padding-left:30px;width:45px;padding-top:15px;"/></a></div>
 </div>
-<div style="margin:0;clear:both;font-size:10px;text-align:center;width:100%;">With Offices in Bryn Mawr, PA and Upper Gwynedd, PA</div>
+<div style="margin:0;clear:both;font-size:10px;text-align:center;width:100%;">With Offices in Bryn Mawr, PA and Princeton, NJ</div>
